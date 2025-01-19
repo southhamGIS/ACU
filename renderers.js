8f61a3f4bf7d2d395807f88cd0e19387dec5c09f
southGIS <jinkuhuang@126.com>
2025-01-19 19:14:49 +0000

    Add files via upload

---
+++ renderers.js
@@ -0,0 +1,5 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See https://js.arcgis.com/4.22/esri/copyright.txt for details.
+//>>built
+define("exports ./renderers/ClassBreaksRenderer ./renderers/DictionaryRenderer ./renderers/DotDensityRenderer ./renderers/HeatmapRenderer ./renderers/Renderer ./renderers/SimpleRenderer ./renderers/UniqueValueRenderer ./renderers/support/jsonUtils ./renderers/support/types".split(" "),function(a,e,f,g,h,b,k,l,c,d){a.ClassBreaksRenderer=e;a.DictionaryRenderer=f;a.DotDensityRenderer=g;a.HeatmapRenderer=h;a.BaseRenderer=b;a.SimpleRenderer=k;a.UniqueValueRenderer=l;a.fromJSON=c.fromJSON;a.read=c.read;
+a.rendererTypes=d.rendererTypes;a.webSceneRendererTypes=d.webSceneRendererTypes;a.isRenderer=function(m){return m instanceof b};Object.defineProperty(a,"__esModule",{value:!0})});
